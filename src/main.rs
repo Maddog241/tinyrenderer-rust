79dae77a1435978d023a41e2569533aee12a93db
Maddog241 <1248826874@qq.com>
2023-02-08 14:55:57 +0800

    add zbuffer

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod model;
 
 use std::mem::swap;
 
-use cgmath::{Vector3, Point2};
+use cgmath::{Vector3, Point2, Point3, InnerSpace};
 use image::Rgb;
 use model::Model;
 use myimage::MyImage;
@@ -13,6 +13,10 @@ const WHITE: Rgb<u8> = Rgb([255, 255, 255]);
 const RED: Rgb<u8> = Rgb([255, 0, 0]);
 const GREEN: Rgb<u8> = Rgb([0, 255, 0]);
 const BLUE: Rgb<u8> = Rgb([0, 0, 255]);
+
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 800;
 
 fn line(image: &mut MyImage, p0: Point2<u32>, p1: Point2<u32>, color: Rgb<u8>) {
     let (mut x0, mut x1) = (p0.x as f64, p1.x as f64);
@@ -55,11 +59,11 @@ fn line(image: &mut MyImage, p0: Point2<u32>, p1: Point2<u32>, color: Rgb<u8>) {
     }
 }
 
-fn barycentric(p: Point2<u32>, a: Point2<u32>, b: Point2<u32>, c: Point2<u32>) -> (f32, f32, f32) {
-    let vec_x = Vector3::new(b.x as f32 - a.x as f32, c.x as f32 - a.x as f32, a.x as f32 - p.x as f32);
-    let vec_y = Vector3::new(b.y as f32 - a.y as f32, c.y as f32 - a.y as f32, a.y as f32 - p.y as f32);
-    let mut uv1 = vec_x.cross(vec_y);
-    if uv1.z == 0.0 {
+fn barycentric(p: Point3<f32>, a: Point3<f32>, b: Point3<f32>, c: Point3<f32>) -> (f32, f32, f32) {
+    let vec_x = Vector3::new(b.x - a.x, c.x - a.x, a.x - p.x);
+    let vec_y = Vector3::new(b.y - a.y, c.y - a.y, a.y - p.y);
+    let mut uv1 = vec_x.cross(vec_y); 
+    if uv1.z.abs() < 1e-2 {
         // in this case, the triangle is degenerate
         (-1.0, 1.0, 1.0)
     } else {
@@ -71,41 +75,63 @@ fn barycentric(p: Point2<u32>, a: Point2<u32>, b: Point2<u32>, c: Point2<u32>) -
 }
 
 
-fn triangle(image: &mut MyImage, a: Point2<u32>, b: Point2<u32>, c: Point2<u32>, color: Rgb<u8>) {
+fn triangle(image: &mut MyImage, zbuffer: &mut Vec<Vec<f32>>, a: Point3<f32>, b: Point3<f32>, c: Point3<f32>, color: Rgb<u8>) {
     let (image_width, image_height) = image.dimensions();
-    let (x_min, x_max) = (a.x.min(b.x.min(c.x)), a.x.max(b.x.max(c.x)).min(image_width-1));
-    let (y_min, y_max) = (a.y.min(b.y.min(c.y)), a.y.max(b.y.max(c.y)).min(image_height-1));
+    let mut bbox_min = Point2::new(image_width as f32 - 1.0, image_height as f32 - 1.0);
+    let mut bbox_max = Point2::new(0.0f32, 0.0f32);
+
+    for point in [a, b, c] {
+        bbox_min.x = bbox_min.x.min(point.x);
+        bbox_min.y = bbox_min.y.min(point.y);
+        bbox_max.x = bbox_max.x.max(point.x);
+        bbox_max.y = bbox_max.y.max(point.y);
+    }
+
+    let (x_min, x_max) = (bbox_min.x as u32, bbox_max.x as u32);
+    let (y_min, y_max) = (bbox_min.y as u32, bbox_max.y as u32);
 
     for x in x_min..(x_max+1) {
         for y in y_min..(y_max+1) {
-            let (u_a, u_b, u_c) = barycentric(Point2::new(x,y), a, b, c);
+            let (u_a, u_b, u_c) = barycentric(Point3::new(x as f32, y as f32, 0.0f32), a, b, c);
             if u_a >= 0.0 && u_b >= 0.0 && u_c >= 0.0 {
-                image.set(x, y, color);
+                let z = u_a * a.z + u_b * b.z + u_c * c.z;
+                if zbuffer[x as usize][y as usize] < z {
+                    zbuffer[x as usize][y as usize] = z;
+                    // image.set(x, y, Rgb([(z * 255.0 / 2.0) as u8; 3]));
+                    image.set(x, y, color);
+                } 
             }
         }
     }
 }
 
+fn world_to_screen(v: Point3<f32>) -> Point3<f32> {
+    Point3::new((v.x+1.0)*(WIDTH-1)as f32/2.0, (v.y+1.0)*(HEIGHT-1)as f32/2.0, v.z+1.0)
+}
+
 fn main() {
-    let (width, height) = (800, 800);
-    let mut image = MyImage::new(width, height);
+    let mut image = MyImage::new(WIDTH, HEIGHT);
 
     let model = Model::new("./obj/african_head.obj");
+    let mut zbuffer = vec![vec![f32::MIN; HEIGHT as usize]; WIDTH as usize];
 
-    for abc in model.faces {
+    for abc in model.faces.iter() {
         let v0 = model.verts[abc[0]];
         let v1 = model.verts[abc[1]];
         let v2 = model.verts[abc[2]];
 
-        let x0 = ((v0.x+1.0)/2.0 * (width-1) as f32) as u32;
-        let y0 = ((v0.y+1.0)/2.0 * (height-1) as f32) as u32;
-        let x1 = ((v1.x+1.0)/2.0 * (width-1) as f32) as u32;
-        let y1 = ((v1.y+1.0)/2.0 * (height-1) as f32) as u32;
-        let x2 = ((v2.x+1.0)/2.0 * (width-1) as f32) as u32;
-        let y2 = ((v2.y+1.0)/2.0 * (height-1) as f32) as u32;
+        let a = world_to_screen(v0);
+        let b = world_to_screen(v1);
+        let c = world_to_screen(v2);
 
-        triangle(&mut image, Point2::new(x0, y0), Point2::new(x1, y1), Point2::new(x2, y2), Rgb([random::<u8>(), random::<u8>(), random::<u8>()]));
+        // lambert's law
+        let n = ((v1 - v0).cross(v2 - v0)).normalize();
+        let intensity = n.dot(Vector3::new(0.0, 0.0, 1.0));
+
+        if intensity > 0.0 {
+            triangle(&mut image, &mut zbuffer, a, b, c, Rgb([(intensity*255.0) as u8; 3]));
+        }
     }
 
-    image.write_img("output.png");
+    image.write_img("output2.png");
 }
--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,8 @@
-use cgmath::Vector3;
+use cgmath::{Vector3, Point3};
 use tobj;
 
 pub struct Model {
-    pub verts: Vec<Vector3<f32>>,
+    pub verts: Vec<Point3<f32>>,
     pub faces: Vec<Vector3<usize>>, 
     pub texcoords: Vec<f32>,
     pub texcoords_indices: Vec<Vector3<usize>>,
@@ -14,7 +14,6 @@ impl Model {
         let file = tobj::load_obj(
             path,
             &tobj::LoadOptions {
-                triangulate: true,
                 ..Default::default()
             }
         );
@@ -31,13 +30,14 @@ impl Model {
         let n_vert = model.mesh.positions.len() / 3; // the number of vertices
         println!("number of vertices: {}", n_vert);
         for chunk in model.mesh.positions.chunks(3) {
-            verts.push(Vector3::new(chunk[0], chunk[1], chunk[2]));
+            verts.push(Point3::new(chunk[0], chunk[1], chunk[2]));
         }
 
         let n_face = model.mesh.indices.len() / 3;
         println!("number of faces: {}", n_face);
+        println!("indices len: {}", model.mesh.indices.len());
+        println!("tex coord indices len: {}", model.mesh.texcoord_indices.len());
         for i in 0..n_face {
-            // 3 vertices for the face
             faces.push(Vector3::new(model.mesh.indices[3*i] as usize, model.mesh.indices[3*i+1] as usize, model.mesh.indices[3*i+2] as usize));
             texcoords_indices.push(Vector3::new(model.mesh.texcoord_indices[3*i] as usize, model.mesh.texcoord_indices[3*i+1] as usize, model.mesh.texcoord_indices[3*i+2] as usize));
         }
